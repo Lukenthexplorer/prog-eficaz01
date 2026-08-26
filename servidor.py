83f7f3bf7b95c4d8d3b9dc666966d778733bb746
lukenthexplorer <lucas.kenji2002@hotmail.com>
2026-08-25 23:24:43 -0300

    Refactor servidor.py to remove unnecessary newline and ensure app runs on host 0.0.0.0 and port 5000

--- servidor.py
+++ servidor.py
@@ -42,6 +42,5 @@ def favorite_note(id):
     db.toggle_favorite(id)
     return redirect(url_for('home'))
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
